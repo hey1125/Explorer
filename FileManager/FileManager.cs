9755b844e191a500d8c5f8d4144129b29294dd0e
daniel valerio <josedanielvaleriocespedes@gmail.com>
2025-05-24 09:47:13 -0600

    1

---
+++ FileManager/FileManager.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System.Net.Http.Headers;
+
+namespace DataPersistestence
+{
+    public class FileManager
+    {
+    public List<Product> ReadProduct()
+        {
+            var products = new List<Product>();
+            return products; 
+        }
+    }
+}
+    
+       
+  
